ba97c00f59c374548a601b677f8bdea121c2a275
ksulakshana02 <sulakshanakaveesha@gmail.com>
2024-12-17 02:26:06 +0530

    fetched apis using react query and axios and create some hooks

--- src/services/appointmentService.js
+++ src/services/appointmentService.js
@@ -2,8 +2,28 @@ import axiosInstance from "@/util/axiosInstance";
 
 export const createAppointment = (data) => axiosInstance.post("/appointments", data);
 
-export const updateAppointmentStatus = (id, data) => axiosInstance.patch(`/appointments/${id}/status`, data);
+export const updateAppointmentStatus = async ({id, data}) => {
+    const response = await axiosInstance.patch(`/appointments/${id}/status`, data);
+    return response.data;
+}
 
 export const getAppointmentByStatus = (status) => axiosInstance.get(`/appointments/status/${status}`);
 
 export const getAppointmentById = (id) => axiosInstance.get(`/appointments/${id}`);
+
+export const getAppointmentByTutorId = async (tutorId) => {
+    const response = await axiosInstance.get(`/appointments/tutor/${tutorId}`);
+    return response.data;
+}
+
+export const deleteAppointmentById = async (id) => {
+    const response = await axiosInstance.delete(`/appointments/${id}`);
+    return response.data;
+};
+
+export const getAppointmentByStatusAndId = async (status, userId) => {
+    const response = await axiosInstance.get(`/appointments/status/${status}`, {
+        params: {userId}
+    });
+    return response.data;
+};
--- src/services/invoiceService.js
+++ src/services/invoiceService.js
@@ -4,4 +4,7 @@ export const createInvoice = (data) => axiosInstance.post('/invoices', data);
 
 export const updateInvoicePayment = (invoiceId, data) => axiosInstance.patch(`/invoices/${invoiceId}/payment`, data);
 
-export const getStudentInvoice = (studentId) => axiosInstance.get(`/students/${studentId}/invoices`);
+export const getStudentInvoice = async (studentId) => {
+    const response = await axiosInstance.get(`/students/${studentId}/invoices`);
+    return response.data;
+}
