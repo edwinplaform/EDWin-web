b43637ea8ba42864387ee63139a476b0ec7253ed
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-02-28 00:34:59 +0530

    add auth.js , middleware.js, SessionAuthProvider.js, route.js

---
+++ src/util/auth.js
@@ -0,0 +1,36 @@
+import {getSession as getClientSession, useSession} from "next-auth/react";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+
+export const getSession = async () => {
+    return await getClientSession();
+};
+
+export const getServerAuthSession = async (req) => {
+    return await getServerSession(authOptions);
+};
+
+export const getCurrentUser = async () => {
+    const session = await getSession();
+    return session?.user || null ;
+}
+
+export const getUserRole = async () => {
+    const user = await getCurrentUser();
+    return user?.role || null;
+}
+
+export const isOnboarding = async () => {
+    const user = await getCurrentUser();
+    return user?.isOnboarding || false;
+}
+
+export const isAuthenticated = async () => {
+    const session = await getSession();
+    return !!session;
+}
+
+export const useCurrentUser = () => {
+    const {data: session} = useSession();
+    return session?.user || null;
+}
