44a8e10b37d9b63bda79e73224130b13dae7acda
Yethum Danith <yethumdanith2002@gmail.com>
2025-03-06 20:40:06 +0530

    Become a tutor push

--- src/app/become-tutor/page.jsx
+++ src/app/become-tutor/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
+// import Link from "next/link";
+// import { UserButton } from "@clerk/nextjs";
 
 import React, { useState } from "react";
 import { Button } from "antd";
